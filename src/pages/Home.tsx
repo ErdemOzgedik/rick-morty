feb4db09c4d03c48ed5aaefc780be5468ebb99a8
ErdemOzgedik <erdemozgedik@gmail.com>
2022-06-10 14:10:01 +0300

    axios instance added and tailwind initialized

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import instance from "../api/instance";
 
 function Home() {
+  const getEpisodes = async () => {
+    try {
+      const response = await instance.get("episode");
+
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getEpisodes();
+  }, []);
+
   return (
     <>
       <main>
-        <h2>Welcome to the homepage!</h2>
+        <h2 className="text-3xl font-bold underline">
+          Welcome to the homepage!
+        </h2>
         <p>You can do this, I believe in you.</p>
       </main>
       <nav>
