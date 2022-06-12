79619abeba82f82b63e0b91fdab7922ca8dab2fe
ErdemOzgedik <erdemozgedik@gmail.com>
2022-06-12 12:17:53 +0300

    get episodes via season change

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Character from "./pages/Character";
 import Episode from "./pages/Episode";
 import Home from "./pages/Home";
 import { getSeasonAsync } from "./redux/seasonSlice";
+import { RootState } from "./redux/store";
 
 function App() {
   const dispatch = useDispatch();
+  const season = useSelector((state: RootState) => state.season);
 
   useEffect(() => {
-    dispatch(getSeasonAsync("1"));
-  }, [dispatch]);
+    dispatch(getSeasonAsync(season.current));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch, season.current]);
 
   return (
     <div className="bg-bg-asset bg-no-repeat bg-cover bg-center h-screen w-screen overflow-scroll">
